86bf8a0f3c368dee82e534ef9038896d3b08bbea
Joseph Marie Alcoy <josephmariealcoy01@gmail.com>
2025-09-16 19:54:36 +0800

    Added jm page

---
+++ app/jm/info.tsx
@@ -0,0 +1,11 @@
+export default function Info(){
+    return(
+    <h1 className= "text-3xl font-bold">Sargu</h1>
+)
+        
+        
+    
+
+        
+    
+}
---
+++ app/jm/page.tsx
@@ -0,0 +1,10 @@
+import Info from "./info";
+export default function Page(){
+    return (
+        <div>
+        <h1 className= "text-3xl font-bold">JM ALCOY</h1>
+        <Info/>
+
+        </div>
+    );
+}
