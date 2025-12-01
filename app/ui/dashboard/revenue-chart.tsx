ee63f4415b3b9d2febd681754c2fcfacb8d19574
Joseph Marie Alcoy <josephmariealcoy01@gmail.com>
2025-12-01 16:20:08 +0800

    Update revenue-chart.tsx

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -4,27 +4,28 @@ import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
 import { fetchRevenue } from '@/app/lib/data';
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+// This component is representational only.
+// For data visualization UI, check out:
+// https://www.tremor.so/
+// https://www.chartjs.org/
+// https://airbnb.io/visx/
+
+export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
   const chartHeight = 350;
-
-  // ✔ Uncommented (Chapter 7)
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
+ 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
-
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
-      {/* <div className="rounded-xl bg-gray-50 p-4"> */}
-      {/* ✔ Uncommented chart code */}
+      {/* NOTE: Uncomment this code in Chapter 7 */}
+
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
@@ -50,7 +51,6 @@ export default async function RevenueChart({
             </div>
           ))}
         </div>
-
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
